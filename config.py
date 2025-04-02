29bc4b87f9b0b6d36c81c955520eb41b5b97bf5d
randomidiot34 <stubentigerzimmer@gmail.com>
2025-04-02 13:51:33 +0200

    Initial Commit

---
+++ config.py
@@ -0,0 +1,12 @@
+#Window
+WIN_WIDTH = 1200
+WIN_HEIGHT = 700
+WIN_CAPTION = "Random Topdown"
+FPS = 60
+
+#Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
---
+++ main.py
@@ -0,0 +1,51 @@
+import pygame
+from sprites import *
+from config import *
+
+class Game:
+    def __init__(self):
+        pygame.init()
+
+        #Set screen stuff
+        self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+        pygame.display.set_caption(WIN_CAPTION)
+        self.clock = pygame.time.Clock()
+
+        #Set groups
+        self.all_sprites = pygame.sprite.LayeredUpdates()
+
+        #Create entities
+
+        #Start game
+        self.running = True
+
+    def events(self):
+        #Handle events
+        for event in pygame.event.get():
+            #Quit
+            if event.type == pygame.QUIT:
+                self.running = False
+
+    def update(self):
+        #Update all sprites
+        self.all_sprites.update()
+        
+    def draw(self):
+        #Draw all sprites
+        self.screen.fill(WHITE)
+        self.all_sprites.draw(self.screen)
+        self.clock.tick(FPS)
+        pygame.display.update()
+
+    def main(self):
+        #Gameloop
+        while self.running:
+            self.events()
+            self.update()
+            self.draw()
+
+game = Game()
+
+game.main()
+
+pygame.quit()
